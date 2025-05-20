4a64a10ff916e5a1e9a1136d0aa9cb68487007a3
quentin <quentin.nicolas.ftn@icloud.com>
2025-05-20 21:35:06 +0200

    Corrige le chemin static et ajoute route pour index

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ const app = express();
 
 // === MIDDLEWARE ===
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public', 'index.html'))); // ← sert tous les fichiers HTML/CSS/JS dans le dossier 'public'
+app.use(express.static(path.join(__dirname, 'public'))); // ← sert tous les fichiers HTML/CSS/JS dans le dossier 'public'
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // === CONNEXION À MONGODB ===
 mongoose.connect(process.env.MONGO_URI, {
