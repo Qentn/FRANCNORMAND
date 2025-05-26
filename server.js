1d8a8a3d7b97b5384b96477f125f26de5184fafb
quentin <quentin.nicolas.ftn@icloud.com>
2025-05-26 13:10:50 +0200

    modifn env

--- server.js
+++ server.js
@@ -230,4 +230,4 @@ app.get('/logout', (req, res) => {
 
 // LANCEMENT DU SERVEUR
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Serveur lancé sur http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Serveur lancé sur http://localhost:${PORT}!`));
